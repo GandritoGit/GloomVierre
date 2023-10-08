861b81f302faf3f6f753194bb5ab27555b6e463d
GandritoGit <gandritogames@gmail.com>
2023-10-08 14:59:10 -0400

    Added font and styled menu

--- Scenes/PauseMenu.gd
+++ Scenes/PauseMenu.gd
@@ -1,15 +1,14 @@
 extends Control
 
 
-
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Campsite.tscn")
-
-
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_credits_pressed():
 	pass # Replace with function body.
 
-
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+
