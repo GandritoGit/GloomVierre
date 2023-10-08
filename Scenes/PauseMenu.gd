1f357232710afbf47d7af108e075cead576aa59e
GandritoGit <gandritogames@gmail.com>
2023-10-08 19:25:31 -0400

    walls done, campsite menu done
    should be finished

--- Scenes/PauseMenu.gd
+++ Scenes/PauseMenu.gd
@@ -4,8 +4,6 @@ extends Control
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
-func _on_credits_pressed():
-	pass # Replace with function body.
 
 func _on_quit_pressed():
 	get_tree().quit()
